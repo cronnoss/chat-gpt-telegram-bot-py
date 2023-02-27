216bd0dbb1f15b45976e6fee69bc5fa2cae28ea5
Evgeniy Karasev <karasevevg@gmail.com>
2023-02-27 14:00:31 +0300

    add logging

--- chat_gpt.py
+++ chat_gpt.py
@@ -6,6 +6,10 @@
 import telebot
 import openai
 import os
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 API_KEY = os.getenv('API_KEY')
@@ -15,6 +19,7 @@ openai.api_key = API_KEY
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
+    logging.info(f'Received message: {message.text}')
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=f"{message.text}",
@@ -23,6 +28,7 @@ def handle_text(message):
         stop=None,
         temperature=0.5,
     )
+    logging.info(f'Sending response: {response.choices[0].text}')
     bot.send_message(message.chat.id, response.choices[0].text)
 
 bot.polling()
